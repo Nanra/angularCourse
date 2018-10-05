07df2e7d0ee91db85ea99569e1a5c8e0f4b05bb9
Nanra <sukedynanra@gmail.com>
2018-10-05 15:57:58 +0700

    Belajar Templeting dan Komponen

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  selector: '[app-servers]', // Selector dijadikan atribut di dalam element HTML
+  // selector: 'app-servers', // Normal pakai selector model ini
+  // templateUrl: './servers.component.html', // Templete berasal dari file HTML (Normal Pakai ini)
+  // Templete pakai element html inline
+  template : `
+  <app-server></app-server>
+  <app-server></app-server>
+  <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
