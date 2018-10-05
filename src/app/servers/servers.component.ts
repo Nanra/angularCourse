ae6f6209c473dae4ec8d4e3a134aaa8def8128dc
Nanra <sukedynanra@gmail.com>
2018-10-05 16:09:56 +0700

    Selektor sebagai tag class html

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector: '[app-servers]', // Selector dijadikan atribut di dalam element HTML
+  // selector: '[app-servers]', // Selector dijadikan atribut di dalam element HTML
+  selector: '.app-servers', // Selecter sebagai Class dalam HTML
   // selector: 'app-servers', // Normal pakai selector model ini
   // templateUrl: './servers.component.html', // Templete berasal dari file HTML (Normal Pakai ini)
   // Templete pakai element html langsung inline
